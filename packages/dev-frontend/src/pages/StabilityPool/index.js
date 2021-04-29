e0d117d2b869a22910cb8b47b87acd49daafb0eb
lukabajic <lukabajic.dev@gmail.com>
2021-04-29 11:42:20 +0200

    Add page route

---
+++ packages/dev-frontend/src/pages/StabilityPool/index.js
@@ -0,0 +1,13 @@
+import classes from "./StabilityPool.module.css";
+
+const Stake = () => {
+  return (
+    <>
+      <div className={classes.tabs}>
+        <div className={classes.tab}>STAKE LUSD</div>
+      </div>
+    </>
+  );
+};
+
+export default Stake;

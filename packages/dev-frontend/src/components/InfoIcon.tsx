031e05a49caf1250ed18148baad3dc83e978f64c
lukabajic <lukabajic.dev@gmail.com>
2021-05-08 20:38:13 +0200

    Tooltip styling

--- packages/dev-frontend/src/components/InfoIcon.tsx
+++ packages/dev-frontend/src/components/InfoIcon.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Tippy, { TippyProps } from "@tippyjs/react";
 import { Icon } from "./Icon";
 import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import classes from "./InfoIcon.module.css";
 
 export type InfoIconProps = Pick<TippyProps, "placement"> &
   Pick<FontAwesomeIconProps, "size"> & {
@@ -10,7 +11,13 @@ export type InfoIconProps = Pick<TippyProps, "placement"> &
 
 export const InfoIcon: React.FC<InfoIconProps> = ({ placement = "right", tooltip, size = "1x" }) => {
   return (
-    <Tippy interactive={true} placement={placement} content={tooltip} maxWidth="268px">
+    <Tippy
+      className={classes.wrapper}
+      interactive={true}
+      placement={placement}
+      content={tooltip}
+      maxWidth="268px"
+    >
       <span>
         &nbsp;
         <Icon name="question-circle" size={size} />
